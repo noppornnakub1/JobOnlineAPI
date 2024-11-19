139050d4bfd19f9775b50414eb1912052485ccd0
Nopporn <sengchip1@gmail.com>
2024-11-19 17:56:34 +0700

    Add DateOptionsController

---
+++ Controllers/DateOptionsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Dapper;
+
+namespace JobOnlineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateOptionsController : ControllerBase
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public DateOptionsController(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        [HttpGet("GetDateOptions")]
+        public async Task<IActionResult> GetDateOptions([FromQuery] int? currentYear)
+        {
+            try
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@CurrentYear", currentYear);
+
+                var dateOptions = await _dbConnection.QueryAsync<DateOption>(
+                    "GetDateSelectionOptions",
+                    parameters,
+                    commandType: CommandType.StoredProcedure
+                );
+
+                return Ok(dateOptions);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = "An error occurred while retrieving date options.",
+                    Error = ex.Message
+                });
+            }
+        }
+    }
+
+    public class DateOption
+    {
+        public required string ThaiMonth { get; set; }
+        public int MonthNumber { get; set; }
+        public int Year { get; set; }
+        public int MaxDay { get; set; }
+    }
+}
