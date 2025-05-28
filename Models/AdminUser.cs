93b39d7f1b5a70eb68986d12ee35dfc68ed52a2b
Noppon Eamkham <sengchip1@gmail.com>
2025-05-28 14:12:08 +0700

    Fix

--- Models/AdminUser.cs
+++ Models/AdminUser.cs
@@ -2,11 +2,11 @@
 {
     public class AdminUser
     {
-        public int AdminID { get; set; } 
+        public int? AdminID { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
         public required string Role { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
         public string? ConfirmConsent { get; set; }
     }
 }
