986926bed54411804ed941ac22be52b60774d188
Noppon Eamkham <sengchip1@gmail.com>
2025-05-28 14:20:26 +0700

    Fix

--- Models/Applicant.cs
+++ Models/Applicant.cs
@@ -2,13 +2,13 @@
 {
     public class Applicant
     {
-        public int ApplicantID { get; set; }
+        public int? ApplicantID { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
         public required string Phone { get; set; }
         public string? Resume { get; set; }
-        public DateTime AppliedDate { get; set; }
+        public DateTime? AppliedDate { get; set; }
         public string? JobTitle { get; set; }
         public string? JobLocation { get; set; }
         public string? JobDepartment { get; set; }
