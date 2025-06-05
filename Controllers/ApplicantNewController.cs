8d51f44aeeb6a12d6841dd61f2de381fa94c874d
Flukelnw <flukelnw1142@gmail.com>
2025-06-05 14:26:18 +0700

    fix

--- Controllers/ApplicantNewController.cs
+++ Controllers/ApplicantNewController.cs
@@ -1175,12 +1175,4 @@ namespace JobOnlineAPI.Controllers
             return (userId, confirmConsent);
         }
     }
-    //public class FileStorageConfig
-    //{
-    //    public string EnvironmentName { get; set; } = "Development";
-    //    public string BasePath { get; set; } = string.Empty;
-    //    public string? NetworkUsername { get; set; }
-    //    public string? NetworkPassword { get; set; }
-    //    public string ApplicationFormUri { get; set; } = string.Empty;
-    //}
 }
