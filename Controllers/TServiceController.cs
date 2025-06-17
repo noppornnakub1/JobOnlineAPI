45046fae0d897e7e7945cf328d91c488ae77ee84
Thanakorn DuangKaew <Thanakorn.Dua@gmmchannelholding.com>
2025-06-17 14:17:50 +0700

    Fix code create New Controller TService

---
+++ Controllers/TServiceController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+
+namespace JobOnlineAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TServiceController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TServiceController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("getTService")]
+        public async Task<IActionResult> GetYearRange()
+        {
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return StatusCode(500, new { message = "Connection string is missing or not configured." });
+            }
+
+            try
+            {
+                using var connection = new SqlConnection(connectionString);
+                var resultTService = await connection.QueryAsync<dynamic>(
+                    "sp_GetAllTService",
+                    commandType: CommandType.StoredProcedure);
+
+                return Ok(resultTService);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    message = "An error occurred while retrieving year range.",
+                    error = ex.Message
+                });
+            }
+        }
+    }
+
+}
