7cc2da9ce69086e6c7567e5582826cddec10ef4e
Noppon Eamkham <sengchip1@gmail.com>
2025-08-22 16:08:00 +0700

    Del Api LoginAdmin

--- Controllers/LoginAdminNewController.cs
+++ Controllers/LoginAdminNewController.cs
@@ -10,46 +10,6 @@ namespace JobOnlineAPI.Controllers
     {
         private readonly DapperContext _context = context;
 
-        [HttpPost]
-        [ProducesResponseType(typeof(IEnumerable<dynamic>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> LoginAdmin([FromBody] LoginRequest request)
-        {
-            try
-            {
-                using var connection = _context.CreateConnection();
-                var parameters = new DynamicParameters();
-                if (string.IsNullOrEmpty(request.Username) || string.IsNullOrEmpty(request.Password))
-                {
-                    return BadRequest("Username and Password are required.");
-                }
-
-                parameters.Add("@Username", request.Username);
-
-                var query = "EXEC sp_GetAdminUsersWithRole @Username";
-                var result = await connection.QueryFirstOrDefaultAsync(query, parameters);
-
-                if (result == null) return Unauthorized("User or password is Invalid.");
-                string hashedPassword = result.Password;
-                bool isPasswordMatch = BCrypt.Net.BCrypt.Verify(request.Password, hashedPassword);
-                if (!isPasswordMatch) return Unauthorized("User or password is Invalid.");
-
-                result.Password = "";
-
-                return Ok(new
-                {
-                    result.AdminID,
-                    result.Username,
-                    result.EMAIL,
-                    result.Role,
-                    result.Department
-                });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
-
         [HttpPost("LoginAD")]
         [ProducesResponseType(typeof(IEnumerable<dynamic>), StatusCodes.Status200OK)]
         public async Task<IActionResult> LoginAdminAD([FromBody] LoginRequestAdmin request)
