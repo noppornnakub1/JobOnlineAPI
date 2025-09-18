0c4fdac191ec80743ada2c4a8148dfb2fe87404b
Flukelnw <flukelnw1142@gmail.com>
2025-09-18 16:54:07 +0700

    add new check codeEmp

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using Dapper;
+using JobOnlineAPI.DAL;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobOnlineAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController(IConfiguration configuration) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpGet("CheckCodeId")]
+        public async Task<IActionResult> CheckCodeId([FromQuery] string? comCode)
+        {
+            try
+            {
+                string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+                using var connection = new SqlConnection(connectionString);
+                var parameters = new DynamicParameters();
+                parameters.Add("@CodeMPID ", comCode);
+                parameters.Add("@Exists", dbType: System.Data.DbType.Boolean, direction: System.Data.ParameterDirection.Output);
+
+                var result = await connection.QueryAsync(
+                    "sp_CheckCodeMPIDExists",
+                    parameters,
+                    commandType: CommandType.StoredProcedure
+                );
+
+                bool Exists = parameters.Get<bool>("@Exists");
+
+                return Ok(Exists);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message });
+            }
+        }
+    }
+}
